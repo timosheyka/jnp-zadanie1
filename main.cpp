305bcdc72a2c2e8a7cb543931df6a1c570de423f
Krzysztof <cackyja@gmail.com>
2022-10-18 19:46:57 +0200

    zaczynam robic ranking

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <regex>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
+
+class ranking{
+    vector<pair<int, int>> top7;
+    unordered_map<int, int> liczba_glosow;
+
+    void dodaj(unordered_set<int> glosy){
+        for (const auto& nr_utworu: glosy) {
+            auto it = liczba_glosow.find(nr_utworu);
+            if (it == liczba_glosow.end())
+                liczba_glosow.insert({nr_utworu, 1});
+            else
+                it->second++;
+        }
+
+        //update top7
+    }
+};
 
 int main() {
     string input_line;
